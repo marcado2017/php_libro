c38ace51ed652b6ea8db765d601712c610cf6b74
clemente <info@blablakasa.com>
2017-12-10 16:46:11 +0000

    cambiado el nombre por versiondephp

---
+++ versiondephp.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h3>Qué versión de php utilizo</h3>
+	<br>
+	<br>
+	<?php 
+		phpinfo()
+	 ?>
+	 <!-- ruta de la imagen del php-->
+	 <img src="images/brand.gif" alt="">
+
+
+</body>
+</html>
